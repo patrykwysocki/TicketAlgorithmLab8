3a4fce976165910f068ccbdee122f4f682125f8c
patrykwysocki <patrykwysocki>
2020-02-02 22:10:03 +0000

    ticket algorithm

--- TicketAlgorithm/TicketAlgorithm/Main.cpp
+++ TicketAlgorithm/TicketAlgorithm/Main.cpp
@@ -1,63 +1,119 @@
-#include <thread>
 #include <iostream>
-#include <queue>
-#include <mutex>
-#include <string>
-#include <array>
 #include <atomic>
-static const int m_processNumbers = 5;
-int m_number = 1;
-int m_next = 1;
-int m_threadNum=0;
-std::mutex m_mutex;
-
-std::array<int, m_processNumbers> m_turn;
-void cs();
-int main()
+#include <thread>
+#include <mutex>
+#include <sstream>
+#include "Vector2D.h"
+class Player
 {
-	for (int i = 0; i < m_turn.size(); i++)
+public:
+	Vector2D m_position;
+	Player() 
 	{
-		m_turn[i] = 0;
+
+	}
+	Player(Vector2D t_position) :
+		m_position(t_position)
+	{
+
 	}
 
-    std::thread t1(cs);
-    std::thread t2(cs);
+};
 
-    t1.join();
-    t2.join();
-	system("pause");
-}
-void cs()
+using namespace std;
+
+const int n = 4;
+int newTicket;
+int currentOrder;
+int activeOrders[n] = { 0,0,0,0 };
+int numThreads;
+mutex m_mutex;
+Vector2D m_position = Vector2D(1.0f, 1.0f);
+Player m_player(m_position);
+
+void func()
 {
-	int i = m_threadNum++;  //Intentional post-increment
-	//lock the mutual exclusion
+	int i = numThreads++;
+	
 	m_mutex.lock();
-	std::cout << "Thread number : " << i << std::endl;
+	cout << "Current Thread: " << i <<  endl;
 	m_mutex.unlock();
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	this_thread::sleep_for(chrono::seconds(1));
 
 	while (true)
 	{
-		//entry protocol
-		m_turn[i] = m_number;
-		m_number ++;
+		activeOrders[i] = newTicket;
+		newTicket++;
+
 		m_mutex.lock();
-		std::cout << "Thread : " << i << " turn " << m_turn[i] << std::endl;
+		cout << "Current Thread: " << i << ".\Order number : " << activeOrders[i] << endl;
+		cout << endl;
 		m_mutex.unlock();
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		//skip
-		while (m_turn[i] != m_next)
+
+		this_thread::sleep_for(chrono::seconds(1));
+
+		while (activeOrders[i] != currentOrder)
 		{
 			continue;
 		}
+		switch (i)
+		{
+		case 0:
+			std::cout << "Render update :" << endl;
+			m_player.m_position.x += 2.0f;
+			break;
+		case 1:
+			std::cout << "Input update :" << endl;
+			m_player.m_position.y += 2.0f;
+			break;
+		case 2:
+			std::cout << "Physics update : " << endl;
+			m_player.m_position.x -= 1.0f;
+			break;
+		case 3:
+			std::cout << "AI update : " << endl;
+			m_player.m_position.y -= 1.0f;
+			break;
+		default:
+			m_player.m_position += 10.0f;
+			break;
+		}
 		m_mutex.lock();
-		std::cout << "Thread : " << i << " critical section " << std::endl;
+		cout << "Player x : " << m_player.m_position.x << "\tPlayer y : " << m_player.m_position.y << endl;
+		cout << "thread number: " << i << "\tnow being served Order number : " << activeOrders[i] << endl;
 		m_mutex.unlock();
-		//critical section
-		m_next ++;
+
+		currentOrder++;;
+
 		m_mutex.lock();
-		std::cout << "Thread : " << i <<" next " << m_next << std::endl;
+		//cout << "next order is " << currentOrder << endl;
 		m_mutex.unlock();
 	}
+}
 
+
+int main()
+{
+	srand(time(NULL));
+
+	numThreads = 0;
+	newTicket = 1;
+	currentOrder = 1;
+	for (int i = 0; i < n; i++)
+	{
+		activeOrders[i] = 0;
+	}
+
+	thread threadRender = thread(func);
+	thread threadInput = thread(func);
+	thread threadPhysics = thread(func);
+	thread threadAI = thread(func);
+
+	threadRender.join();
+	threadInput.join();
+	threadPhysics.join();
+	threadAI.join();
+
+	return 0;
 }
